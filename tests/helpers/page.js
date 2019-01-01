f4f0a0f0ad2181f7b4e796bb25a1f8b4feabfcdb
Raffy <r4faelsimoes@gmail.com>
2019-01-01 15:45:50 +1100

    travis

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -4,9 +4,7 @@ const userFactory = require("../factories/userFactory");
 
 class CustomPage {
   static async build() {
-    const browser = await puppeteer.launch({
-      headless: true
-    });
+    const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
 
     const page = await browser.newPage();
     const customPage = new CustomPage(page);
